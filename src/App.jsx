273b775aa6b23426f70381d1b558887c6f48efb2
Sugatraj <sugatraj.2106@gmail.com>
2025-03-24 12:30:40 +0530

    Refactor Tailwind CSS setup and update package configurations
    - Removed `@tailwindcss/vite` and adjusted the `vite.config.js` to exclude it from plugins.
    - Updated `postcss.config.js` to directly use `tailwindcss` instead of `@tailwindcss/postcss`.
    - Revised `package.json` to reflect the correct version of `tailwindcss` and removed unnecessary dependencies.
    - Enhanced `src/index.css` with Tailwind's directive syntax for better organization and added custom styles for body.

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App() {
               {categories.map(({ name }) => (
                 <Tab
                   key={name}
-                  className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
+                  className="cursor-pointer rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
                 >
                   {name}
                 </Tab>
